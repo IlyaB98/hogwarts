e3b3cd8bb220454d6e29d34c93417aa7da78e865
Ilya <b.ilya.98@mail.ru>
2023-02-15 13:44:24 +0300

    Поправил замечания по дз логгирование и конфигурирование в приложении

--- src/main/java/ru/hogwarts/school/service/InfoService.java
+++ src/main/java/ru/hogwarts/school/service/InfoService.java
@@ -1,5 +1,19 @@
 package ru.hogwarts.school.service;
 
-public interface InfoService {
-    int getPort();
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InfoService {
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    private static final Logger logger = LoggerFactory.getLogger(InfoService.class);
+    public int getPort() {
+        logger.info("Was invoked method for get port");
+        return serverPort;
+    }
 }
